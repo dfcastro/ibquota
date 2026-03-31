5c0a1d7cbbf67a00d1d63ba9224a0e1f8a3ac09c
dfcastro <dfcastro@outlook.com.br>
2026-03-31 11:22:31 -0300

    Configura .gitignore para proteger senhas de banco de dados

---
+++ includes/db.sample.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ARQUIVO DE EXEMPLO PARA O GIT
+ * Instruções: Copie este ficheiro, renomeie para db.php e coloque os seus dados reais.
+ */ 
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'ibquota3';
+
+// Restante do seu código de conexão mysqli ou PDO vem aqui embaixo...
+// $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+?>
--- includes/header.php
+++ includes/header.php
@@ -1,123 +1,124 @@
 <?php
+
 /**
  * IBQUOTA 3
  * GG - Gerenciador Grafico do IBQUOTA
- * 
- * 29/10/2018 - Valcir C.
- *
- * Cabecalho das paginas. Com o menu.
- */ 
+ * * Cabecalho das paginas. Com o menu.
+ * Atualizado para Bootstrap 5 e otimizado via CDN.
+ */
 
 if (file_exists("css")) {
   $path_raiz = "";
 } else {
   $path_raiz = "../";
 }
-
-
 ?>
 
 <!DOCTYPE html>
-<html lang="pt">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php echo $path_raiz;?>css/bootstrap.min.css" type="text/css" /> 
-      
-    <link rel="icon" href="/favicon.png" />
-    <meta name="description" content="Controle de Quota de Impressão">
+<html lang="pt-BR">
 
-    <title>Quota de Impressão (IBQUOTA 3)</title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Quota de Impressão (IBQUOTA 3)</title>
+  <meta name="description" content="Controle de Quota de Impressão">
+  <link rel="icon" href="/favicon.png" />
 
-<script>
-$(function() {
-    $( "#txtDataInicial" ).datepicker();
-});
-</script>   
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-   <nav class="navbar navbar-expand-lg navbar-light bg-success shadow">
-  <a class="navbar-brand" href="#"><b>IBQuota 3</b></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+</head>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo $path_raiz;?>index.php">Home</a>
-      </li>
-<?php
- if ($_SESSION['permissao'] > 0){
-?>
-      <li class="nav-item dropdown active">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Cadastros
-        </a>
-        <div class="dropdown-menu bg-success" aria-labelledby="navbarDropdown" >
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>usuarios/">Usu&aacute;rios</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>grupos/">Grupos</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>usuarios/usuario_quota_add.php">Quota adicional</a>
-        </div>
-      </li>
-<?php
- }
-?>
-      <li class="nav-item dropdown active">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Relat&oacute;rios
-        </a>
-        <div class="dropdown-menu bg-success" aria-labelledby="navbarDropdown" >
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>relatorios/impressoes.php">Impress&otilde;es</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>relatorios/impressoes_detalhadas.php">Impress&otilde;es detalhadas</a>
-		  <a class="dropdown-item" href="<?php echo $path_raiz;?>relatorios/auditoria_impressoes.php">Auditoria Impress&otilde;es</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>relatorios/impressoes_com_erro.php">Impress&otilde;es com erro</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>relatorios/usuarios.php">Pesquisar quota de usu&aacute;rio</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>relatorios/quota_por_usuario.php">Quota por usu&aacute;rio</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>relatorios/ibquota_logs.php">Logs do CUPS</a>
-        </div>
-      </li> 
+<body>
 
-<?php
- if ($_SESSION['permissao'] == 2){
-?>
-      <li class="nav-item dropdown active">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Avan&ccedil;ado
-        </a>
-        <div class="dropdown-menu bg-success" aria-labelledby="navbarDropdown" >
+  <script>
+    // Mantivemos o seu script original. Certifique-se de que o jQuery está a ser 
+    // carregado noutro ficheiro (como no footer) para isto funcionar corretamente.
+    $(function() {
+      $("#txtDataInicial").datepicker();
+    });
+  </script>
 
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>configuracao.php">Configura&ccedil;&atilde;o Geral </a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>politicas/">Pol&iacute;ticas de Impress&atilde;o</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>politicas/init_quota_politica.php">Inicializa Quota de Impress&atilde;o</a>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow-sm mb-4">
+    <div class="container-fluid">
+      <a class="navbar-brand fw-bold" href="#">IBQuota 3</a>
 
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>adm_users/">Usu&aacute;rios Administrativos</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>test_ldap.php">Teste de conex&atilde;o LDAP</a>
-        </div>
-      </li>
-<?php
- }
-?>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    </ul>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?php echo $path_raiz; ?>index.php">Home</a>
+          </li>
 
-    <ul class="navbar-nav navbar-right">
-        <li class="nav-item dropdown active">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Conta
-        </a>
-        <div class="dropdown-menu bg-success" aria-labelledby="navbarDropdown" >
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>trocarsenha.php">Trocar Senha</a>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>ajuda.php">Ajuda</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="<?php echo $path_raiz;?>includes/logout.php">Sair</a>
-        </div>
-      </li>
-     </ul>
-  </div>
-</nav>
-<br><br>
+          <?php if ($_SESSION['permissao'] > 0) { ?>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" href="#" id="dropCadastros" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Cadastros
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="dropCadastros">
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>usuarios/">Usuários</a></li>
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>grupos/">Grupos</a></li>
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>usuarios/usuario_quota_add.php">Quota adicional</a></li>
+              </ul>
+            </li>
+          <?php } ?>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="dropRelatorios" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Relatórios
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="dropRelatorios">
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>relatorios/impressoes.php">Impressões</a></li>
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>relatorios/impressoes_detalhadas.php">Impressões detalhadas</a></li>
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>relatorios/auditoria_impressoes.php">Auditoria Impressões</a></li>
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>relatorios/impressoes_com_erro.php">Impressões com erro</a></li>
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>relatorios/usuarios.php">Pesquisar quota de usuário</a></li>
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>relatorios/quota_por_usuario.php">Quota por usuário</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>relatorios/ibquota_logs.php">Logs do CUPS</a></li>
+            </ul>
+          </li>
+
+          <?php if ($_SESSION['permissao'] == 2) { ?>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" href="#" id="dropAvancado" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Avançado
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="dropAvancado">
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>configuracao.php">Configuração Geral</a></li>
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>politicas/">Políticas de Impressão</a></li>
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>politicas/init_quota_politica.php">Inicializa Quota de Impressão</a></li>
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>adm_users/">Usuários Administrativos</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>test_ldap.php">Teste de conexão LDAP</a></li>
+              </ul>
+            </li>
+          <?php } ?>
+        </ul>
+
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="dropConta" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              ⚙️ Conta
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropConta">
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>trocarsenha.php">Trocar Senha</a></li>
+              <li><a class="dropdown-item" href="<?php echo $path_raiz; ?>ajuda.php">Ajuda</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item text-danger" href="<?php echo $path_raiz; ?>includes/logout.php">Sair</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
